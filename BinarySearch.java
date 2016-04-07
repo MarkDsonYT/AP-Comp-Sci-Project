275bf2719300cfbbb982ad2e6b34f7b6191d2acb
Mark Danison <darktacos123@gmail.com>
2016-04-07 19:06:37 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,18 @@
-public class BinarySearch{}
+//assumes array is already sorted!
+
+public class BinarySearch
+{
+  public static int findVal(int[] arr, int val)
+  {
+    int bot = 0;
+    int top = arr.length - 1;
+    while(bot <= top)
+    {
+      int mid = (bot + top)/2;
+      if(arr[mid] == val) return mid;
+      else if ( arr[mid] < val) bot = mid + 1;
+      else top = mid -1;
+    }
+    return -1;
+  }
+}
