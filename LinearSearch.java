4a2eb399bab93aa3784d927d340ca659a8fdd525
Mark Danison <darktacos123@gmail.com>
2016-04-07 19:06:50 -0400

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,1 +1,13 @@
-public class LinearSearch{}
+import java.util.Arrays;
+
+public class LinearSearch
+{
+  public static int search(int[] arr, int val)
+  {
+    for(int i = 0; i < arr.length; i++)
+    {
+      if(arr[i] == val) return i;
+    }
+    return -1;
+  }
+}
