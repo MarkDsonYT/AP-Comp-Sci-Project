a0efe5ec041c7399967901f319ed13019e8837eb
Mark Danison <darktacos123@gmail.com>
2016-04-09 19:37:42 -0400

    Minified BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,18 +1,1 @@
-//assumes array is already sorted!
-
-public class BinarySearch
-{
-  public static int findVal(int[] arr, int val)
-  {
-    int bot = 0;
-    int top = arr.length - 1;
-    while(bot <= top)
-    {
-      int mid = (bot + top)/2;
-      if(arr[mid] == val) return mid;
-      else if ( arr[mid] < val) bot = mid + 1;
-      else top = mid -1;
-    }
-    return -1;
-  }
-}
+public class BinarySearch{public static int findVal(int[]arr,int val){int bot=0;int top=arr.length-1;while(bot<=top){int mid=(bot+top)/2;if(arr[mid]==val)return mid;else if(arr[mid]<val)bot=mid+1;else top=mid-1;}return-1;}}
