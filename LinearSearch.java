08fcd04bfc02e07ede9606aa9f34b4425e651300
Mark Danison <darktacos123@gmail.com>
2016-04-09 19:38:36 -0400

    Minified LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,13 +1,1 @@
-import java.util.Arrays;
-
-public class LinearSearch
-{
-  public static int search(int[] arr, int val)
-  {
-    for(int i = 0; i < arr.length; i++)
-    {
-      if(arr[i] == val) return i;
-    }
-    return -1;
-  }
-}
+public class LinearSearch{public static int search(int[]arr,int val){for(int i=0;i<arr.length;i++){if(arr[i]==val)return i;}return-1;}}
