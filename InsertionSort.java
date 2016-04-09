32dea88302073f8449496bb4321f3e18ba26a05d
Mark Danison <darktacos123@gmail.com>
2016-04-09 19:38:19 -0400

    Minified InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,13 +1,1 @@
-public class InsertionSort{
-  public static void insertionSort(int[] arr){
-    for(int i = 1; i<arr.length;++i){
-    int val = arr[i];
-    int j = i;
-    while(j>0&&val<arr[j-1]){
-      arr[j]=arr[j-1];
-      j--;
-    }
-    arr[j]=val;
-    }
-  }
-}
+public class InsertionSort{public static void insertionSort(int[]arr){for(int i=1;i<arr.length;++i){int val=arr[i];int j=i;while(j>0&&val<arr[j-1]){arr[j]=arr[j-1];j--;}arr[j]=val;}}}
